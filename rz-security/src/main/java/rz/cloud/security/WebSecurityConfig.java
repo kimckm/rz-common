053edce64498b5b52fe786baeeb1ec43c498de2f
Kim <331599158@qq.com>
2020-08-07 15:41:14 +0800

    增加.editorconfig

--- rz-security/src/main/java/rz/cloud/security/WebSecurityConfig.java
+++ rz-security/src/main/java/rz/cloud/security/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package rz.cloud.security;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -15,8 +18,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .httpBasic()
+                .and()
                 .formLogin().permitAll()
                 .and()
-                .logout().permitAll();
+                .logout().disable();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername("rz")
+                .password("{bcrypt}$2a$10$87eaM3lUqEs3sj5g0Rxl7erbK2wosUfWG5gl4wPll7iFuaTEjNROK")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
 }
