9164243436c53a5970cb986f9e45ccf39ad47077
Kim <331599158@qq.com>
2020-09-06 16:47:46 +0800

    增加OAuth2服务

---
+++ rz-pure/src/main/java/rz/cloud/pure/MySecurityConfig.java
@@ -0,0 +1,18 @@
+package rz.cloud.pure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/login").permitAll().and()
+			.authorizeRequests()
+			.anyRequest().hasRole("USER");
+	}
+
+}
