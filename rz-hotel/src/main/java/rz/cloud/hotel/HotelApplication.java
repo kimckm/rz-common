5ba654c254f2779eec33b9ccebe9c7d0df114f76
Kim <331599158@qq.com>
2020-10-21 14:23:56 +0800

    fixbug

--- rz-hotel/src/main/java/rz/cloud/hotel/HotelApplication.java
+++ rz-hotel/src/main/java/rz/cloud/hotel/HotelApplication.java
@@ -3,8 +3,8 @@ package rz.cloud.hotel;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@MapperScan("rz.cloud.web.mapper")
-@EnableDiscoveryClient
-@SpringBootApplication
+// @MapperScan("rz.cloud.web.mapper")
+// @EnableDiscoveryClient
+// @SpringBootApplication
 public class HotelApplication {
 }
