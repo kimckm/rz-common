c94681978ebb8f718b3bdf31ed804fa099bf84b8
Kim <331599158@qq.com>
2021-01-13 07:51:38 +0800

    增加填空题数据表设计

--- rz-exam/src/main/resources/schema-mariadb.sql
+++ rz-exam/src/main/resources/schema-mariadb.sql
@@ -4,7 +4,6 @@ create table t_choice (
   question varchar(255) comment '题目',
   create_at datetime comment '创建时间'
 ) engine=InnoDB default charset=utf8mb4;
-
 insert into t_choice(id, question, create_at) values(1, '中国有多少个省份?', '2020-12-27 11:53:00');
 
 drop table if exists t_exam;
@@ -13,12 +12,21 @@ create table t_exam (
   title varchar(255) comment '考试标题',
   create_at datetime comment '创建时间'
 ) engine=InnoDB default charset=utf8mb4;
-
 insert into t_exam(id, title, create_at) values(1, '模拟考试', '2020-12-27 11:53:00');
 
-  drop table if exists t_exam;
-  create table t_exam (
-    id int primary key,
-    title varchar(255) comment '考试标题',
-    create_at datetime comment '创建时间'
-  ) engine=InnoDB default charset=utf8mb4;
+drop table if exists t_completion;
+create table t_completion (
+  id int primary key,
+  question varchar(255) comment '题目',
+  create_at datetime comment '创建时间'
+) engine=InnoDB default charset=utf8mb4;
+insert into t_completion(id, question, create_at) values(1, '苹果的英文单词是{s}。', '2020-12-27 11:53:00');
+
+drop table if exists t_completion_option;
+create table t_completion_option (
+  id int primary key,
+  fillId int comment '所属填空题',
+  code varchar(20) comment '占位代码',
+  answer varchar(20) comment '答案'
+) engine=InnoDB default charset=utf8mb4;
+insert into t_completion_option(id, fillId, code, answer) values(1, 1, 's', '/^apple$/i');
