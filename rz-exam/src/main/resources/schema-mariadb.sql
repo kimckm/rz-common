386799f5ac175b3432ace4ba0bcbd98c6c3b87e7
Kim <331599158@qq.com>
2021-01-14 18:00:50 +0800

    增加填空题数据

--- rz-exam/src/main/resources/schema-mariadb.sql
+++ rz-exam/src/main/resources/schema-mariadb.sql
@@ -21,7 +21,12 @@ create table t_completion (
   correct varchar(255) comment '正确答案',
   create_at datetime comment '创建时间'
 ) engine=InnoDB default charset=utf8mb4;
-insert into t_completion(id, question, correct, create_at) values(1, '苹果的英文单词是{s}。', '[{"code":"s","expected":"/^apple$/i"}]', '2020-12-27 11:53:00');
+insert into
+  t_completion(id, question, correct, create_at)
+values
+  (1, '苹果的英文单词是{s}。', '[{"code":"s","expected":"/^apple$/i"}]', '2020-12-27 11:53:00'),
+  (2, 'TCP传输控制协议(Transmission Control Protocol)，是一个面向{A}的协议，为用户进程提供可靠的{B}字节流。', '[{"code":"A","expected":"连接"},{"code":"B","expected":"全双工"}]', '2020-12-27 11:53:00')
+;
 
 --drop table if exists t_completion_option;
 --create table t_completion_option (
