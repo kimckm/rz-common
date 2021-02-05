d868b7f6e67a23e3dfe8f247a1ea34aa5f3c0082
Kim <331599158@qq.com>
2021-02-05 14:57:57 +0800

    调整启动配置

--- rz-exam/src/main/java/rz/exam/ExamApplication.java
+++ rz-exam/src/main/java/rz/exam/ExamApplication.java
@@ -1,36 +1,15 @@
 package rz.exam;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.util.ResourceUtils;
-import rz.exam.controller.CompletionController;
-import rz.exam.controller.dto.CompletionSaveDTO;
-
-import java.io.File;
-import java.util.List;
 
 @Slf4j
 @SpringBootApplication
 public class ExamApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext run = SpringApplication.run(ExamApplication.class);
-		CompletionController completionController = run.getBean(CompletionController.class);
-
-		ObjectMapper om = new ObjectMapper();
-		try {
-			File file = ResourceUtils.getFile("classpath:data.json");
-			List<CompletionSaveDTO> list = om.readValue(file, new TypeReference<List<CompletionSaveDTO>>() {});
-			for (CompletionSaveDTO dto : list) {
-				completionController.save(dto);
-			}
-		} catch (Exception e) {
-			log.error("", e);
-		}
+		SpringApplication.run(ExamApplication.class);
 	}
 
 }
--- rz-exam/src/main/resources/schema-mariadb.sql
+++ rz-exam/src/main/resources/schema-mariadb.sql
@@ -1,32 +1,27 @@
-drop table if exists t_choice;
 create table t_choice (
   id int primary key,
   question varchar(255) comment '题目',
   create_at datetime comment '创建时间'
 ) engine=InnoDB default charset=utf8mb4 comment='选择题';
 
-drop table if exists t_exam;
 create table t_exam (
   id int primary key auto_increment,
   title varchar(255) comment '考试标题',
   create_at datetime comment '创建时间'
 ) engine=InnoDB default charset=utf8mb4 comment='考试表';
 
-drop table if exists t_exam_question;
 create table t_exam_question (
   id int primary key auto_increment,
   exam_id int,
   question_id int
 ) engine=InnoDB default charset=utf8mb4 comment='考试题目表';
 
-drop table if exists t_completion;
 create table t_completion (
   id int primary key auto_increment,
   question varchar(500) comment '题目',
   create_at datetime comment '创建时间'
 ) engine=InnoDB default charset=utf8mb4 comment='填空题';
 
-drop table if exists t_completion_correct;
 create table t_completion_correct (
  id int primary key auto_increment,
  completion_id int comment '所属填空题',
@@ -34,7 +29,6 @@ create table t_completion_correct (
  expected varchar(20) comment '答案'
 ) engine=InnoDB default charset=utf8mb4 comment='填空题正确答案';
 
-drop table if exists t_completion_audio;
 create table t_completion_audio (
  id int primary key auto_increment,
  completion_id int comment '所属填空题',
