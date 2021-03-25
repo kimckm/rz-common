9d36c0f83895e3c64f5f1aa2bac022cd0dcb20a5
Kim <331599158@qq.com>
2021-03-25 14:14:48 +0800

    增加spring boot admin

--- rz-exam/src/main/java/rz/exam/ExamApplication.java
+++ rz-exam/src/main/java/rz/exam/ExamApplication.java
@@ -6,15 +6,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Slf4j
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ExamApplication {
 
