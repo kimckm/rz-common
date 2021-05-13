4056c0ed3f98d35342f26d97252d7da148836d3f
Kim <331599158@qq.com>
2021-05-14 07:27:39 +0800

    a

--- rz-exam/src/main/java/rz/exam/controller/ChoiceController.java
+++ rz-exam/src/main/java/rz/exam/controller/ChoiceController.java
@@ -4,15 +4,19 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import rz.exam.controller.dto.ChoiceSaveDTO;
 import rz.exam.controller.dto.PageQueryDTO;
 import rz.exam.controller.dto.PageResultDTO;
 import rz.exam.model.Choice;
+import rz.exam.model.ChoiceOption;
+import rz.exam.service.ChoiceOptionService;
 import rz.exam.service.ChoiceService;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 
 @RequestMapping("/choices")
@@ -22,6 +26,8 @@ public class ChoiceController {
 
 	@Autowired
 	private ChoiceService choiceService;
+	@Autowired
+	private ChoiceOptionService choiceOptionService;
 
 	@GetMapping
 	public Object list(PageQueryDTO pageQueryDTO) {
@@ -39,7 +45,14 @@ public class ChoiceController {
 
 	@GetMapping("/{id}")
 	public Object findOne(@PathVariable Long id) {
-		return choiceService.getById(id);
+		Choice choice = choiceService.getById(id);
+		ChoiceSaveDTO dto = new ChoiceSaveDTO();
+		BeanUtils.copyProperties(choice, dto);
+		List<ChoiceOption> choiceOptions = choiceOptionService.list(Wrappers.lambdaQuery(ChoiceOption.class)
+			.eq(ChoiceOption::getChoiceId, id)
+		);
+		dto.setChoiceOptions(choiceOptions);
+		return dto;
 	}
 
 	@PostMapping
